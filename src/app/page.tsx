c51832053c7af91df10b1ba42bb7825079aede44
sonani <sonani3136@gmail.com>
2025-08-19 12:30:15 +0900

    logo_2
    깃허브 로고 끝

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Home, Folder, Mail, Phone, MapPin, Code } from "lucide-react";
+import { Home, Folder, Mail, Phone, MapPin, Code, Github } from "lucide-react";
 import Image from "next/image";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -69,129 +69,159 @@ export default function Page() {
       </nav>
 
       {/* Main Content */}
-      <main className="flex-1 p-10 max-w-7xl mx-auto">
-        <section id="about" className="py-20">
-          <h1 className="section-title mb-5">손한이</h1>
-          
-          <h2 className="mb-5">{ADDRESS} | {TEL} | {EMAIL}</h2>
-          <p className="mb-0">시시각각 변화하는 IT 산업에 빠르게 적응해나갈 수 있게 부단히 노력하고 있습니다.</p>
-          <p className="mb-0">사용자의 입장에서 생각하여 맥락을 파악하고, 더 나은 서비스를 만들기 위한 고민을 멈추지 않겠습니다. </p>
-        </section>
-
-        <hr className="my-10 border-gray-300" />
-
-        <section id="skills" className="py-20">
-          <h1 className="section-title">Skills</h1>
-          <div className="skill-card flex flex-wrap justify-center items-center gap-2">
-            {skillsData.map((skill, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center justify-center p-1"
-              >
-                <Image
-                  src={skill.logo}
-                  alt={skill.name}
-                  width={90}
-                  height={28}
-                  sizes="90px"
-                  className="object-contain h-[28px] w-[90px]"
-                  unoptimized
-                  
-                />
+      <main className="flex-1 overflow-y-auto">
+        <div>
+          <div id="about" className="max-w-7xl mx-auto p-10">
+            <section className="py-20">
+              <h1 className="text-8xl font-bold mb-5">손한이</h1>
+              <h2 className="mb-9">
+                {ADDRESS} | {TEL} | {EMAIL}
+              </h2>
+              <div className="mission-statement mt-8">
+                <p className="mb-0">
+                  시시각각 변화하는 IT 산업에 빠르게 적응해나갈 수 있게 부단히
+                  노력하고 있습니다.
+                </p>
+                <p className="mb-0">
+                  사용자의 입장에서 생각하여 맥락을 파악하고, 더 나은 서비스를
+                  만들기 위한 고민을 멈추지 않겠습니다.
+                </p>
               </div>
-            ))}
+            </section>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-2 mt-4 text-gray-400 dark:hover:text-black hover:text-white transition-colors"
+            >
+              <div className="bg-gray-800 dark:bg-gray-200 rounded-full p-2">
+              <Github className="h-9 w-9" />
+              </div>
+            </a>
           </div>
-        </section>
 
-        <hr className="my-10 border-gray-300" />
-        
-        <section id="projects" className="py-20">
-          <h1 className="section-title">Projects</h1>
-          <div className="space-y-8">
-            {projectsData.map((project, index) => (
-              <div key={index} className="main-card">
-                <h2 className="text-xl font-semibold">
-                  {project.project_title}
-                </h2>
-                <p className="mb-2">{project.description}</p>
-                {project.youtube_embed ? (
-                  <div className="aspect-video bg-gray-200 flex items-center justify-center mb-4">
-                    <iframe
-                      width="100%"
-                      height="100%"
-                      src={project.youtube_embed}
-                      title={project.project_title}
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    ></iframe>
+          <div id="skills" className="max-w-7xl mx-auto p-10">
+            <section className="py-20">
+              <h1 className="section-title">Skills</h1>
+              <div className="flex flex-wrap justify-baselines items-center gap-2">
+                {skillsData.map((skill, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col items-center justify-center p-1"
+                  >
+                    <Image
+                      src={skill.logo}
+                      alt={skill.name}
+                      width={90}
+                      height={28}
+                      sizes="90px"
+                      className="object-contain h-[28px] w-[90px]"
+                      unoptimized
+                    />
                   </div>
-                ) : (
-                  <div className="aspect-video bg-gray-200 flex items-center justify-center mb-4">
-                    YouTube Embed 자리
+                ))}
+              </div>
+            </section>
+          </div>
+
+          <hr className="my-10 border-gray-300" />
+
+          <div id="projects" className="max-w-7xl mx-auto p-10">
+            <section className="py-20">
+              <h1 className="section-title">Projects</h1>
+              <div className="space-y-8">
+                {projectsData.map((project, index) => (
+                  <div key={index} className="main-card">
+                    <h2 className="text-xl font-semibold">
+                      {project.project_title}
+                    </h2>
+                    <p className="mb-2">{project.description}</p>
+                    {project.youtube_embed ? (
+                      <div className="aspect-video bg-gray-200 flex items-center justify-center mb-4">
+                        <iframe
+                          width="100%"
+                          height="100%"
+                          src={project.youtube_embed}
+                          title={project.project_title}
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                        ></iframe>
+                      </div>
+                    ) : (
+                      <div className="aspect-video bg-gray-200 flex items-center justify-center mb-4">
+                        YouTube Embed 자리
+                      </div>
+                    )}
+                    <SyntaxHighlighter
+                      language="javascript"
+                      style={atomDark}
+                      customStyle={{ borderRadius: "0.25rem" }}
+                    >
+                      {project.code}
+                    </SyntaxHighlighter>
+                    <a
+                      href={project.github_link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex items-center gap-2 mt-2 text-gray-400 dark:hover:text-white hover:text-black transition-colors"
+                    >
+                      <Github className="h-5 w-5" />
+                      <span>GitHub Repository</span>
+                    </a>
                   </div>
-                )}
-                <SyntaxHighlighter
-                  language="javascript"
-                  style={atomDark}
-                  customStyle={{ borderRadius: "0.25rem" }}
-                >
-                  {project.code}
-                </SyntaxHighlighter>
-                <a
-                  href={project.github_link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sky-600 underline mt-2 inline-block"
-                >
-                  GitHub 링크
-                </a>
+                ))}
               </div>
-            ))}
+            </section>
           </div>
-        </section>
 
-        <hr className="my-10 border-gray-300" />
+          <hr className="my-5 border-gray-300" />
 
-        <section id="contact" className="py-20">
-          <h1 className="section-title">Contact</h1>
-          <div className="flex flex-row gap-8 items-start">
-            {/* Left Side: Contact Info */}
-            <div className="w-1/2 space-y-4 main-card">
-              <div className="flex items-center gap-2">
-                <Mail className="h-5 w-5 text-slate-500" />
-                <span>{EMAIL}</span>
-                <button
-                  onClick={() => handleCopy(EMAIL)}
-                  className="copy-button"
-                >
-                  {copiedText === EMAIL ? "복사됨!" : "복사"}
-                </button>
-              </div>
-              <div className="flex items-center gap-2">
-                <Phone className="h-5 w-5 text-slate-500" />
-                <span>{TEL}</span>
-                <button onClick={() => handleCopy(TEL)} className="copy-button">
-                  {copiedText === TEL ? "복사됨!" : "복사"}
-                </button>
-              </div>
-              <div className="flex items-center gap-2">
-                <MapPin className="h-5 w-5 text-slate-500" />
-                <span>{ADDRESS}</span>
-                <button
-                  onClick={() => handleCopy(ADDRESS)}
-                  className="copy-button"
-                >
-                  {copiedText === ADDRESS ? "복사됨!" : "복사"}
-                </button>
-              </div>
-            </div>
+          <div id="contact" className="max-w-7xl mx-auto p-10">
+            <section className="py-20">
+              <h1 className="section-title">Contact</h1>
+              <div className="flex flex-row gap-8 items-start">
+                {/* Left Side: Contact Info */}
+                <div className="w-1/2 space-y-4 main-card">
+                  <div className="flex items-center gap-2">
+                    <Mail className="h-5 w-5 text-slate-500" />
+                    <span>{EMAIL}</span>
+                    <button
+                      onClick={() => handleCopy(EMAIL)}
+                      className="copy-button"
+                    >
+                      {copiedText === EMAIL ? "복사됨!" : "복사"}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Phone className="h-5 w-5 text-slate-500" />
+                    <span>{TEL}</span>
+                    <button
+                      onClick={() => handleCopy(TEL)}
+                      className="copy-button"
+                    >
+                      {copiedText === TEL ? "복사됨!" : "복사"}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MapPin className="h-5 w-5 text-slate-500" />
+                    <span>{ADDRESS}</span>
+                    <button
+                      onClick={() => handleCopy(ADDRESS)}
+                      className="copy-button"
+                    >
+                      {copiedText === ADDRESS ? "복사됨!" : "복사"}
+                    </button>
+                  </div>
+                </div>
 
-            {/* Right Side: Map */}
-            <div className="w-1/2 h-96">
-              <KakaoMap latitude={35.803142} longitude={128.740039} />
+                {/* Right Side: Map */}
+                <div className="w-1/2 h-96">
+                  <KakaoMap latitude={35.803142} longitude={128.740039} />
+                </div>
               </div>
+            </section>
           </div>
-        </section>
+        </div>
       </main>
     </div>
   );
